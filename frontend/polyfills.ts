edaf201da0774393479a2b8cfa1c10aaa233971c
Manju-nad-K <nadkmanju@gmail.com>
2025-10-28 10:37:02 +0530

    Create polyfills.ts

---
+++ frontend/polyfills.ts
@@ -0,0 +1,29 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This file is divided into 2 sections:
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ *      file.
+ *
+ * The current setup is for "evergreen" browsers; the last versions of browsers that automatically
+ * update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera), Edge >= 13 on
+ * desktop, and iOS 10 and Chrome on mobile.
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
